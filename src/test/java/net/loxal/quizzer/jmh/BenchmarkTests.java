0c386e7106ee28fd33b5f6c78bf6347d2696ae17
Alex <alexander.orlov@loxal.net>
2017-03-04 12:30:25 +0100

    cleanup

--- src/test/java/net/loxal/quizzer/jmh/BenchmarkTests.java
+++ src/test/java/net/loxal/quizzer/jmh/BenchmarkTests.java
@@ -7,7 +7,9 @@ package net.loxal.quizzer.jmh;
 import net.loxal.quizzer.VoteTests;
 import net.loxal.quizzer.controller.VoteController;
 import net.loxal.quizzer.dto.Vote;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -19,31 +21,29 @@ import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@State(Scope.Benchmark)
 public class BenchmarkTests {
     private final static Logger LOG = LoggerFactory.getLogger(BenchmarkTests.class);
 
     private static final Properties PROPERTIES = new Properties();
-    private static String USER_NAME;
-    private static String USER_PASSWORD;
+    private static String userName;
+    private static String userPassword;
 
     static {
         try {
             PROPERTIES.load(BenchmarkTests.class.getResourceAsStream("/application.properties"));
-            USER_NAME = PROPERTIES.getProperty("security.user.name");
-            USER_PASSWORD = PROPERTIES.getProperty("security.user.password");
+            userName = PROPERTIES.getProperty("security.user.name");
+            userPassword = PROPERTIES.getProperty("security.user.password");
         } catch (IOException e) {
             LOG.error(e.getMessage());
         }
     }
 
     @BenchmarkMode(Mode.SingleShotTime)
-    @Threads(5)
     @Benchmark
     public void benchmark() throws Exception {
         final TestRestTemplate testRestTemplate = new TestRestTemplate();
         final ResponseEntity<Vote> vote = testRestTemplate
-                .withBasicAuth(USER_NAME, USER_PASSWORD)
+                .withBasicAuth(userName, userPassword)
                 .postForEntity("http://localhost:8200" + VoteController.ENDPOINT, VoteTests.EXPECTED_MULTIPLE_ANSWERS_CORRECT, Vote.class);
 
         assertThat(vote.getStatusCode()).isEqualTo(HttpStatus.CREATED);
