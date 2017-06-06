e6b9a091d3da546b63b1b65a662f178193b9f771
Alex <alexander.orlov@loxal.net>
2017-06-06 18:02:09 +0200

    fix deployed version

--- src/main/java/net/loxal/quizzer/Application.java
+++ src/main/java/net/loxal/quizzer/Application.java
@@ -26,6 +26,7 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.vault.annotation.VaultPropertySource;
@@ -110,8 +111,9 @@ public class Application extends WebSecurityConfigurerAdapter {
                         "/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs"
                 ).permitAll()
                 .anyRequest().authenticated()
-//                .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
-                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+//                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().logout().logoutSuccessUrl("/start.html").permitAll()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .ignoringAntMatchers("/polls/**", "/certificates/**", "/votes/**")
                 .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
